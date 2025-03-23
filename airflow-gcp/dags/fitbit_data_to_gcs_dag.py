cf5049611a6e665951e24e205ba1b29aaac361d1
Michael Salata <michael.d.salata@gmail.com>
2025-03-22 20:05:20 -0500

    fixed uploading example data to the wrong place in the bucket

--- airflow-gcp/dags/fitbit_data_to_gcs_dag.py
+++ airflow-gcp/dags/fitbit_data_to_gcs_dag.py
@@ -21,7 +21,7 @@ GCP_GCS_BUCKET = str(os.environ.get("GCP_GCS_BUCKET", f"{PROJECT_ID}-fitbit-buck
 BIGQUERY_DATASET = str(os.environ.get("BIGQUERY_DATASET", "fitbit_dataset"))
 # CREDENTIALS_FILE = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "google_credentials.json")
 
-local_home_path = os.environ.get("AIRFLOW_HOME")
+airflow_path = os.environ.get("AIRFLOW_HOME")
 
 
 def upload_to_gcs(bucket_name, max_retries=3):
@@ -33,33 +33,32 @@ def upload_to_gcs(bucket_name, max_retries=3):
     CHUNK_SIZE = 8 * 1024 * 1024
 
     # fitbit_data_regex = ["*.parquet"]
-    regex = "*.parquet"
-
+    regex = "*.parquet"     # currently download_locally.py dumps it's data in the dags folder
     data_files = glob.glob(regex)
     if len(data_files) == 0:
         print("No Data Files Found -> uploading example data")
-        regex = "/opt/airflow/example_data/" + regex
+        regex = os.path.join(airflow_path,"example_data","*.parquet")
+        data_files = glob.glob(regex)
     
-    for blob_name in glob.glob(regex):
-        blob = bucket.blob(blob_name)
+    for file_w_path in data_files:
+        file_name = os.path.basename(file_w_path)
+        blob = bucket.blob(file_name)
         blob.chunk_size = CHUNK_SIZE
         
         for attempt in range(max_retries):
             try:
-                print(f"Uploading {blob_name} to {bucket_name} (Attempt {attempt + 1}/{max_retries})...")
-                blob.upload_from_filename(blob_name)
+                print(f"Uploading {file_w_path} to {bucket_name} (Attempt {attempt + 1}/{max_retries})...")
+                blob.upload_from_filename(file_w_path)
                 
-                if storage.Blob(bucket=bucket, name=blob_name).exists(client):
-                    print(f"Upload Verification successful for gs://{bucket_name}/{blob_name}")
+                if blob.exists():
+                    print(f"Upload Verification successful for gs://{bucket_name}/{file_name}")
                     break
                 else:
-                    print(f"Verification failed for {blob_name}, retrying...")
+                    print(f"Verification failed for {file_w_path}, retrying...")
             except Exception as e:
-                print(f"Failed to upload {blob_name} to GCS: {e}")
+                print(f"Failed to upload {file_w_path} to GCS: {e}")
             
-            time.sleep(1)
-            
-    return
+            time.sleep(2)
 
 
 default_args = {
@@ -74,14 +73,14 @@ with DAG(
     schedule_interval="@monthly",
     default_args=default_args,
     catchup=False,
-    max_active_runs=1,
+    max_active_runs=3,
     tags=['dtc-de'],
 ) as dag:
     download_locally_task = PythonOperator(
         task_id="download_locally_task",
         python_callable=download_past_6_months,
         op_kwargs={
-            "tokens_path": local_home_path,
+            "tokens_path": airflow_path,
         },
     )
 
@@ -89,7 +88,7 @@ with DAG(
         task_id="fitbit_to_parquet_task",
         python_callable=profile_sleep_heartrate_jsons_to_parquet,
         op_kwargs={
-            "base_path": local_home_path,
+            "base_path": airflow_path,
         },
     )
 
@@ -148,7 +147,7 @@ with DAG(
 
     dbt_transforms_task = BashOperator(
         task_id='dbt_transforms_task',
-        bash_command=f'cd {local_home_path}/dbt_resources && dbt build',
+        bash_command=f'cd {airflow_path}/dbt_resources && dbt build',
         trigger_rule="all_success"
     )
 
