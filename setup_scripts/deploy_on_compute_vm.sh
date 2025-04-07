3f5293af29ec6a09f443acb6c2f16311245e95e0
Michael Salata <michael.d.salata@gmail.com>
2025-04-06 20:07:09 -0500

    deploy waits for docker.dsock avail

--- setup_scripts/deploy_on_compute_vm.sh
+++ setup_scripts/deploy_on_compute_vm.sh
@@ -4,6 +4,9 @@ sudo groupadd -f docker
 # TODO pull the var.ssh_user from .tfvars instead of $(whoami)
 sudo usermod -aG docker $(whoami)
 
+echo "Waiting for Docker socket to be available..."
+while [ ! -S /var/run/docker.sock ]; do sleep 1; done
+
 sudo chown root:docker /var/run/docker.sock
 sudo chmod 660 /var/run/docker.sock
 
@@ -11,11 +14,11 @@ newgrp docker <<EOF
 if [ ! -d "compare-my-biometrics" ]; then
   # clone and update repo
   git clone https://github.com/MichaelSalata/compare-my-biometrics.git
-  cp ./terraform.tfvars ./compare-my-biometrics/terraform.tfvars
-  bash update_vm_env.sh
-else
-  echo "Repo already cloned."
-fi
+fi  
+
+cp ./terraform.tfvars ./compare-my-biometrics/terraform/terraform.tfvars
+bash update_vm_env.sh
+
 
 cd ./compare-my-biometrics/airflow-gcp
 mkdir ./logs ./plugins ./config
