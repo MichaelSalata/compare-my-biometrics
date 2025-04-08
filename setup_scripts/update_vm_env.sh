3cedaeb4fbd849ff656d70df8a1b18a9c74bbc05
Michael Salata <michael.d.salata@gmail.com>
2025-04-08 11:26:56 -0500

    formatting

--- setup_scripts/update_vm_env.sh
+++ setup_scripts/update_vm_env.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Define file paths
 TFVARS_FILE=~/compare-my-biometrics/terraform/terraform.tfvars
 ENV_FILE=~/compare-my-biometrics/airflow-gcp/.env
 
-# Extract values from terraform.tfvars
 project=$(grep '^project' "$TFVARS_FILE" | awk -F'=' '{print $2}' | tr -d ' "')
 ssh_user=$(grep '^ssh_user' "$TFVARS_FILE" | awk -F'=' '{print $2}' | tr -d ' "')
 bucket_name=$(grep '^gcs_bucket_name' "$TFVARS_FILE" | awk -F'=' '{print $2}' | tr -d ' "')
 
-# Construct variables
 AIRFLOW_UID=$(id -u)
 GOOGLE_CREDENTIALS="/home/${ssh_user}/google_credentials.json"
 GCP_PROJECT_ID="$project"
 GCP_GCS_BUCKET="$bucket_name"
 # GCP_GCS_BUCKET="${project}-fitbit-bucket"
 
-# Helper: update or insert key-value pair in .env
+# insert/overwrite key-value pairs in .env
 update_env_var() {
     local key=$1
     local value=$2
@@ -27,7 +24,6 @@ update_env_var() {
     fi
 }
 
-# Update required variables
 update_env_var "AIRFLOW_UID" "$AIRFLOW_UID"
 update_env_var "GOOGLE_CREDENTIALS" "$GOOGLE_CREDENTIALS"
 update_env_var "GCP_PROJECT_ID" "$GCP_PROJECT_ID"
