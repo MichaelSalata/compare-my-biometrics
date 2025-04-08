310c6eb87affe5b3f6f4602e19ae3d590e827595
Michael Salata <michael.d.salata@gmail.com>
2025-04-08 11:50:26 -0500

    fixed bug not downloading fitbit data

--- setup_scripts/deploy_on_compute_vm.sh
+++ setup_scripts/deploy_on_compute_vm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo snap install docker
 sudo groupadd -f docker
-# TODO pull the var.ssh_user from .tfvars instead of $(whoami)
 sudo usermod -aG docker $(whoami)
 
 sudo chown root:docker /var/run/docker.sock
@@ -10,12 +9,12 @@ sudo chmod 660 /var/run/docker.sock
 newgrp docker <<EOF
 if [ ! -d "compare-my-biometrics" ]; then
   # clone and update repo
-  git clone https://github.com/MichaelSalata/compare-my-biometrics.git
-  cp ./terraform.tfvars ./compare-my-biometrics/terraform.tfvars
-  bash update_vm_env.sh
-else
-  echo "Repo already cloned."
-fi
+  git clone --branch cloud-depoyment --single-branch --depth 1 https://github.com/MichaelSalata/compare-my-biometrics.git
+fi  
+
+cp -f ./terraform.tfvars ./compare-my-biometrics/terraform/terraform.tfvars
+cp -f ./fitbit_tokens.json ./compare-my-biometrics/airflow-gcp/dags/fitbit_tokens.json
+bash update_vm_env.sh
 
 cd ./compare-my-biometrics/airflow-gcp
 mkdir ./logs ./plugins ./config
