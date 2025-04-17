efc718fad7622e98ee9a6edfc47fc789f5b11b9b
Michael Salata <michael.d.salata@gmail.com>
2025-04-17 11:12:52 -0500

    generalized deployment script

--- setup_scripts/deploy_on_compute_vm.sh
+++ setup_scripts/deploy_on_compute_vm.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+github_project_name="compare-my-biometrics"
+github_repo_name="https://github.com/MichaelSalata/${github_project_name}.git"
+git_branch="main"
+
 sudo snap install docker
 sudo groupadd -f docker
 sudo usermod -aG docker $(whoami)
@@ -10,15 +14,15 @@ sudo chown root:docker /var/run/docker.sock
 sudo chmod 660 /var/run/docker.sock
 
 newgrp docker <<EOF
-if [ ! -d "compare-my-biometrics" ]; then
-  git clone --branch cloud-migration --single-branch --depth 1 https://github.com/MichaelSalata/compare-my-biometrics.git
-fi  
+if [ ! -d "${github_project_name}" ]; then
+  git clone --branch ${git_branch} ${github_repo_name}
+fi
 
-cp -f ./terraform.tfvars ./compare-my-biometrics/terraform/terraform.tfvars
-cp -f ./fitbit_tokens.json ./compare-my-biometrics/airflow-gcp/dags/fitbit_tokens.json
+cp -f ./terraform.tfvars ./${github_project_name}/terraform/terraform.tfvars
+cp -f ./fitbit_tokens.json ./${github_project_name}/airflow-gcp/dags/fitbit_tokens.json
 bash update_vm_env.sh
 
-cd ./compare-my-biometrics/airflow-gcp
+cd ./${github_project_name}/airflow-gcp
 mkdir ./logs ./plugins ./config
 DOCKER_BUILDKIT=1 docker compose build
 docker compose up -d airflow-init && docker compose up
