30cec595a66ee9fe94f7d1651372030dc34e714e
Michael Salata <michael.d.salata@gmail.com>
2025-06-23 17:47:56 -0500

    added dag run process example data

--- airflow-gcp/dags/parallel_backfill_fitbit_example_data.py
+++ airflow-gcp/dags/parallel_backfill_fitbit_example_data.py
@@ -2,19 +2,19 @@ import os
 import json
 import subprocess
 import logging
+import glob
 
 from datetime import datetime
 
 from google.cloud import bigquery
 
-from airflow.decorators import dag, task, task_group
+from airflow.decorators import dag, task
 from airflow.models import Connection
 from airflow.utils.db import provide_session
-from airflow.operators.python import get_current_context
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
 from fitbit_json_to_parquet import flatten_fitbit_json_file
-from fitbit_hook import FitbitHook
+
 
 PROJECT_ID = str(os.environ.get("GCP_PROJECT_ID"))
 GCP_GCS_BUCKET = str(os.environ.get("GCP_GCS_BUCKET", f"{PROJECT_ID}-fitbit-bucket"))
@@ -76,55 +76,23 @@ def update_fitbit_connection(session=None):
     task_logger.info("Connection 'fitbit_default' created successfully.")
 
 
-@task
-def prep_example_profile():
-    example_profile_path = "./example_data/profile.json"
-    with open(example_profile_path, 'r') as file:
-        profile_json = json.load(file)
-        if profile_json and profile_json.get("user") and profile_json["user"].get("memberSince"):
-            signup_date_str = profile_json["user"]["memberSince"]
-            signup_date = datetime.strptime(signup_date_str, "%Y-%m-%d")
-            fitbit_hook = FitbitHook()
-            return {
-                "json_file": fitbit_hook.save_data(data=profile_json, endpoint_name="profile"),
-                "signup_date": signup_date
-            }
-        else:
-            raise Exception("empty profile API response")
-
-@task
-def get_profile_path(profile_data: dict):
-    return profile_data["json_file"]
-
-
-@task(retries=3)
-def download_since_signup(signup_date: datetime, endpoint_id: str):
-    fitbit_hook = FitbitHook()
-    end_date = get_current_context()['execution_date'].date()
-    if endpoint_id in fitbit_hook.dayrange_endpoints:
-        response = fitbit_hook.fetch_daterange(endpoint_id=endpoint_id, start=signup_date, end=end_date)
-        if response:
-            return fitbit_hook.save_data(response, endpoint_id, start_date=signup_date, end_date=end_date)
-        else:
-            task_logger.warning(f"No data retrieved for {endpoint_id} from {signup_date} to {end_date}")
-            return None
 
 
 @task
 def flatten_fitbit_data(json_file: str):
     parquet_file, endpoint_id = flatten_fitbit_json_file(json_file=json_file)
-    return {"filename": parquet_file, "endpoint_id": endpoint_id}
+    return {"filepath": parquet_file, "endpoint_id": endpoint_id}
 
 
 @task
 def upload_to_gcs(endpoint_file: dict):
     endpoint_id = endpoint_file["endpoint_id"]
-    filename = endpoint_file["filename"]
+    filename = os.path.basename(endpoint_file["filepath"])
 
     gcp_blob = f"{endpoint_id}/{filename}"
     task_logger.info(f"Uploading {filename} to {gcp_blob}...")
     gcs_hook = GCSHook()
-    gcs_hook.upload(bucket_name=GCP_GCS_BUCKET, object_name=gcp_blob, filename=filename)
+    gcs_hook.upload(bucket_name=GCP_GCS_BUCKET, object_name=gcp_blob, filename=endpoint_file["filepath"])
     task_logger.info(f"Upload successful to {gcp_blob}")
     return gcp_blob
 
@@ -145,9 +113,12 @@ def parquets_to_bq_table(endpoint_id: str):
     return endpoint_id
 
 @task
-def prep_biometric_jobs(profile_data: dict, biometrics: list[str]):
-    signup_date = profile_data["signup_date"]
-    return [{"signup_date": signup_date, "biometric": b} for b in biometrics]
+def find_biometric_jsons(biometrics: list[str]):
+    matched_files = []
+    for b in biometrics:
+        matched_files.extend(glob.glob(f"./example_data/{b}*.json"))
+
+    return matched_files
 
 
 @task
@@ -174,13 +145,13 @@ default_args = {
 }
 
 @dag(
-    dag_id="parallel_backfill_fitbit_from_signup",
+    dag_id="parallel_backfill_fitbit_example_data",
     default_args=default_args,
     schedule=None,
     catchup=False,
     tags=['dtc-de']
 )
-def fitbit_pipeline():
+def fitbit_example_data_pipeline():
     # the only fitbit data currently supported is both:
         # keyword mapped in FitbitHook.py plugin class
         # AND
@@ -189,20 +160,13 @@ def fitbit_pipeline():
     BQ_TABLES = FITBIT_BIOMETRICS + ["profile"]
     update_fitbit_connection()
 
-    # download, flatten and upload profile
-    profile_data = prep_example_profile()
-    flattened_profile = flatten_fitbit_data(json_file=get_profile_path(profile_data))
+    # flatten and upload profile
+    flattened_profile = flatten_fitbit_data(json_file="./example_data/profile.json")
     profile_parquets_in_gcs = upload_to_gcs(flattened_profile)
 
-    # download, flatten and upload 1 biometric datatype
-    @task_group
-    def ETL_biometrics(signup_date: datetime, biometric: str):
-        biometric_json = f"./example_data/{biometric}.json"
-        biometric_parquet = flatten_fitbit_data(json_file=biometric_json)
-        upload_to_gcs(biometric_parquet)
-    
-    # run the task_group for each biometric in parallel
-    biometrics_in_gcs = ETL_biometrics.expand_kwargs(prep_biometric_jobs(profile_data, FITBIT_BIOMETRICS))
+    # flatten and upload biometrics
+    flattened_biometrics = flatten_fitbit_data.expand(json_file=find_biometric_jsons(FITBIT_BIOMETRICS))
+    biometrics_in_gcs = upload_to_gcs.expand(endpoint_file=flattened_biometrics)
 
     # create BigQuery external tables for parquet files in GCS
     setup_bq_ext_tables = parquets_to_bq_table.expand(endpoint_id=BQ_TABLES)
@@ -211,4 +175,4 @@ def fitbit_pipeline():
     [profile_parquets_in_gcs, biometrics_in_gcs] >> setup_bq_ext_tables >> run_dbt()
     
 
-fitbit_dag = fitbit_pipeline()
+fitbit_dag = fitbit_example_data_pipeline()
